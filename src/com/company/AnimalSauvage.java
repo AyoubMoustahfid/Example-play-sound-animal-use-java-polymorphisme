1a5c89f1b3626c9a6e822325348087167b36f8f3
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2020-12-08 18:17:54 +0100

    new push : add caractére in class Animal

--- src/com/company/AnimalSauvage.java
+++ src/com/company/AnimalSauvage.java
@@ -5,22 +5,36 @@ import java.io.File;
 import java.io.IOException;
 
 public class AnimalSauvage extends Animal {
+   
+	
+	
+	public AnimalSauvage(String name, String country, int age) {
+		super(name, country, age);
+		// TODO Auto-generated constructor stub
+	}
 
-    public void ReadSound(String effectSource) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	public void ReadSound(String effectSource) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(effectSource));
         Clip clip = AudioSystem.getClip();
         clip.open(audioIn);
         clip.start();
+        System.out.println("--------------------------------------");
         System.out.println(effectSource + " sound is start ! ");
         System.out.println("s : " + clip.isActive());
-        while (clip.isActive()) {
-            System.out.println("hola "+effectSource);
-        }
+        do{
+        	//System.out.println("hola "+effectSource);
+        }while(clip.isActive());
+        
         clip.close();
         System.out.println(effectSource + " sound is end ! ");
     }
 
 	
-
+  
 	
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,13 @@ package com.company;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
+import java.io.IOException; 
 
 public class Main {
 
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-	 AnimalSauvage lion = new AnimalSauvage();
+	 AnimalSauvage lion = new AnimalSauvage("lion", "african", 30);
+     System.out.println(lion.toString());
 	 lion.ReadSound("C:\\Users\\Admin\\Desktop\\Animal\\src\\lioncub2.wav");
 
 	
